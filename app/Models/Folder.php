3f1bae501e6fb6964a7e9f87fec66677ff92b466
yukarihoshino <yuukaar.2@gmail.com>
2023-11-06 16:44:05 +0900

    タスク一覧追加

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Folder extends Model
 {
     // use HasFactory;
+
+    public function tasks()
+    {
+        return $this->hasMany('App\Models\Task');
+    }
 }
