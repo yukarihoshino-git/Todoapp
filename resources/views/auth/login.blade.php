452e48abc76dceb12d8b364abc2ce858884029a0
yukarihoshino <yuukaar.2@gmail.com>
2024-01-12 16:15:29 +0900

    ログイン更新

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
       <div class="col col-md-offset-3 col-md-6">
         <nav class="panel panel-default">
-          <div class="panel-heading">ログイン</div>
+          <div class="panel-heading">ログイン画面</div>
           <div class="panel-body">
             @if($errors->any())
               <div class="alert alert-danger">
